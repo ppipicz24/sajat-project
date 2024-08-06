dec7912972d898618ec2706ef50a20d5189b6a2e
Pipicz Petra <ppipicz24@gmail.com>
2024-08-06 13:28:23 +0200

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeadersComponent } from './headers/headers.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadersComponent
+    HeadersComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
