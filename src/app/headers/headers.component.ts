d2fdcbf6f479c5385a7406b17afb9365cbb0da69
Pipicz Petra <ppipicz24@gmail.com>
2024-08-06 15:09:43 +0200

    header

--- src/app/headers/headers.component.ts
+++ src/app/headers/headers.component.ts
@@ -6,27 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './headers.component.css'
 })
 export class HeadersComponent {
-  // navbar = document.getElementById('navbar');
 
-  // constructor() { }
-
-  // ngOnInit() {
-  //   window.addEventListener('scroll', this.scroll, true);
-  // }
-
-  // ngOnDestroy(): void {
-  //   //Called once, before the instance is destroyed.
-  //   //Add 'implements OnDestroy' to the class.
-  //   window.removeEventListener('scroll', this.scroll, true);
-  // }
-
-  // scroll = (event): void => {
-  //   if(window.scrollY > this.navbar.offsetTop) {
-  //     this.navbar.classList.add('sticky');
-  //   }
-  //   else {
-  //     this.navbar.classList.remove('sticky');
-  //   }
-  // }
 
 }
