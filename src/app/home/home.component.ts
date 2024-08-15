3011cf497adc3977f29b3bd851157e6cb87251cf
Pipicz Petra <ppipicz24@gmail.com>
2024-08-15 08:05:33 +0200

    home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
+import { User } from '../auth/user.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  isAuth = false;
+  private userSub: Subscription;
 
+  user: User
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuth = !!user;
+    });
+
+    this.user = this.authService.user.value;
+    console.log(this.user);
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.userSub.unsubscribe();
+  }
 }
