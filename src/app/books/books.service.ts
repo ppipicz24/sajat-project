9acca8c41ad29da05b703944fa9349987f11fd33
Pipicz Petra <ppipicz24@gmail.com>
2024-08-21 12:38:20 +0200

    műveletek

--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -14,10 +14,29 @@ export class BooksService {
         return this.books.slice();
     }
 
+    getBook(index: number){
+        return this.books[index];
+    }
+
     setBooks(books: Book[]){
         this.books = books;
         this.booksChanged.next(this.books.slice());
     }
-    
+
+    addBook(book: Book){
+        this.books.push(book);
+        this.booksChanged.next(this.books.slice());
+    }
+
+    updateBook(index: number, newBook: Book){
+        this.books[index] = newBook;
+        this.booksChanged.next(this.books.slice());
+    }
+
+    deleteBook(index: number){
+        this.books.splice(index, 1);
+        this.booksChanged.next(this.books.slice());
+    }
+
 
 }
