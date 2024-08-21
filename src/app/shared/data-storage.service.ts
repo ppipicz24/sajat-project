c5b82becbbf70da0937813886c544ed7aeecaf28
Pipicz Petra <ppipicz24@gmail.com>
2024-08-21 12:26:47 +0200

    data-storage

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BooksService } from "../books/books.service";
+import { tap } from "rxjs";
+import { Book } from "../books/books.model";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService{
+    constructor(private http: HttpClient, private bookService: BooksService){}
+    
+    StoreBooks()
+    {
+        const books = this.bookService.getBooks()
+
+        this.http.put('https://own-project-fd626-default-rtdb.europe-west1.firebasedatabase.app/books.json', books).subscribe(responseData =>{
+            console.log(responseData)
+        })
+    }
+
+    FetchBooks()
+    {
+        this.http.get<Book[]>('https://own-project-fd626-default-rtdb.europe-west1.firebasedatabase.app/books.json').pipe(tap(books=>{
+            return books.map(book =>{
+                return {
+                    ...book
+                }
+            })
+        }),
+        tap(books=>{
+            this.bookService.setBooks(books)
+        })
+    )
+    }
+}
+
